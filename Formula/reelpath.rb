637548942e6012136eab3f6e898ed5ddeddec3ea
Nick Gerace <nickagerace@gmail.com>
2021-08-02 01:59:37 -0400

    Update to 1.2.1

--- Formula/reelpath.rb
+++ Formula/reelpath.rb
@@ -1,8 +1,8 @@
 class Reelpath < Formula
   desc "CLI tool that prints the absolute path for a given file or directory."
   homepage "https://nickgerace.dev"
-  url "https://github.com/nickgerace/reelpath/archive/1.1.0.tar.gz"
-  sha256 "d00c44bc6c7c952e76ac6484860bb8410bb2cd7a6248360334246812c57460ce"
+  url "https://github.com/nickgerace/reelpath/archive/1.2.1.tar.gz"
+  sha256 "c60ac7eb194e0414e1464b890695c9b0a9f88ea5b6022daf40a7b911b86363f5"
   license "Apache-2.0"
 
   depends_on "rust" => :build
